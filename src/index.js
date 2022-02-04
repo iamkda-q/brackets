5b0c17015900b0f6795d5357f21b91a072f62f7a
KDA <iamkda@yandex.ru>
2022-02-04 18:36:41 +0300

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsConfigJoin = bracketsConfig.map(item => item[0] + item[1]);
+  const leftArr = bracketsConfig.map(item => item[0]);
+  const rightArr = bracketsConfig.map(item => item[1]);
+  const strArr = str.split("");
+  for (let i = 0; i < strArr.length; i++) {
+    if (rightArr.includes(strArr[i]) && !leftArr.includes(strArr[i])) return false; // для исключения одной закрывающей скобки (учитывая что есть прямые скобки)
+    if (leftArr.includes(strArr[i]) && (strArr[i + 1]) && rightArr.includes(strArr[i + 1]) && (bracketsConfigJoin.includes(strArr.slice(i, i + 2).join('')))) {
+      strArr.slice(i, i + 2);
+      strArr.splice(i, 2);
+      i = -1;
+    }
+  }
+  if (strArr.length == 0) {
+    return true
+  }
+  return false 
 }
